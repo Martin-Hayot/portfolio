21ad48bbe4dc7a994f82bca41796381d8ae05c55
Martin-Hayot <m.hayot@students.ephec.be>
2025-01-14 22:39:23 +0100

    feat: update dependancies + astro version 5 + refactor homepage

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue(), react()]
 });
