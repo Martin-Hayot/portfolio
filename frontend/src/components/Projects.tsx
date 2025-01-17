a71ab53e2380a47c7d0a29f6e39a7a03396892f9
Martin-Hayot <m.hayot@students.ephec.be>
2025-01-17 19:01:44 +0100

    fix opacity project parallax

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -15,15 +15,18 @@ const Projects = () => {
         offset: ["start end", "end end"],
     });
 
-    const opacity = useTransform(opacityScroll, [0, 0.35], [0, 1]);
+    const opacity = useTransform(opacityScroll, [0, 0.3], [0, 1]);
+
+    // make opacity 0 from a certain treshold and go to 1
+    const opacityMin = useTransform(opacity, [0.5, 1], [0, 1]);
 
     return (
         <motion.section
             id="projects"
             ref={container}
             initial={{ opacity: 0 }}
-            style={{ opacity: opacity }}
-            className="md:mb-64 pt-64 bg-gradient-to-b from-neutral-950 via-violet-900/15 via-[80%] to-transparent"
+            style={{ opacity: opacityMin }}
+            className="md:mb-64 pt-32 bg-gradient-to-b from-neutral-950 via-violet-900/15 via-[80%] to-transparent"
         >
             {projects.map((project, index) => {
                 const targetScale = 1 - (projects.length - index) * 0.05;
