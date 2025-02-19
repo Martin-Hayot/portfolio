df74a070295faad3af93df4d77f312241f772d21
Martin-Hayot <m.hayot@students.ephec.be>
2025-02-19 02:13:14 +0100

    fix: smoot scroll

--- frontend/src/scripts/lenis.ts
+++ frontend/src/scripts/lenis.ts
@@ -4,12 +4,3 @@ import "lenis/dist/lenis.css";
 const lenis = new Lenis({
     autoRaf: true,
 });
-
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
-
-lenis.on("scroll", (e) => {});
